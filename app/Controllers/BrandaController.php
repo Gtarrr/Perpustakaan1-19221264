3e46e8d9031f2489e45b5ddc9f5d97772b9c16b2
mascoy13 <145321806+mascoy13@users.noreply.github.com>
2023-09-25 13:43:39 +0700

    menambahkan fitur beranda

---
+++ app/Controllers/BrandaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BrandaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+    public function home (){
+
+    }
+    public function about(){
+
+    }
+    public function pesan(){
+        
+    }
+        
+    
+}
