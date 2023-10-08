63b476d2e0305baa0aa21c0f19db2701c5db5271
Ulpiainur13 <ulfiahainurwafa@gmail.com>
2023-10-08 17:56:31 +0700

    fitur penerbit

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends BaseController
     {
         //
     }
-
+    
     public function daftar(){
 
     }
@@ -22,5 +22,4 @@ class LoginController extends BaseController
     public function lupapassword(){
 
     }
-
 }
